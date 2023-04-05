a1adc2aecc05a08aaa52a32962b4e2efb2627de1
Canthar90 <d.cieslak90@gmail.com>
2023-04-05 11:39:18 +0200

    Added click animations and code is more refactored

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = []; 
 
 
+function playSound(soundName){
+    var currSound = new Audio("sounds/" + soundName + ".mp3");
+    currSound.play();
+}
+
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 150);
+}
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+    
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("button, "+  "." + randomChosenColour).fadeOut(180).fadeIn(180);
+
+    playSound(randomChosenColour)
 }
 
-var gamePattern = [];
 
-var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour = buttonColours[nextSequence()];
+nextSequence()
 
-gamePattern.push(randomChosenColour);
 
-$("button, "+  "." + randomChosenColour).fadeOut(180).fadeIn(180);
 
-alert(randomChosenColour + ".mp3")
 
-var currSound = new Audio("sounds/" + randomChosenColour + ".mp3");
-currSound.play();
+$("div .btn").click(function (){
+    var colorName = $(this).attr("id");
+    console.log(colorName);
+    
+    playSound(colorName);
+    animatePress(colorName);
+    
+
+    userClickedPattern.push(colorName)
+    
+});
